1da4655752274ad902d099edfe38ce58eed91482
shaun.nichols51@gmail.com <shaun.nichols51@gmail.com>
2014-04-24 20:48:03 -0700

    Starting the Project

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubDropbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 26;
+            string name = "Shaun";
+            Console.WriteLine("My name is {0} and I am {1} years old", name, age);
+        }
+    }
+}
